3630361651141786d36ab5a9ab327c42506a27c7
SarmilaRoy <sarmilaroy583032@gmail.com>
2022-08-23 17:00:02 +0600

    API authentication with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DummyAPIController;
 use App\Http\Controllers\DeviceController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +22,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('data',[DummyAPIController::class,'getData']);
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    //All secure URL's
+    //Route::apiResource("member",[MemberController::class]);
+    Route::get('data',[DummyAPIController::class,'getData']);
 // Route::get('list/{id?}',[DeviceController::class,'list']);
 
 Route::get('list',[DeviceController::class,'list']);
@@ -35,3 +40,24 @@ Route::delete('delete/{id}',[DeviceController::class,'delete']);
 
 
 Route::post('save',[DeviceController::class,'testData']);
+    });
+
+// Route::get('data',[DummyAPIController::class,'getData']);
+// // Route::get('list/{id?}',[DeviceController::class,'list']);
+
+// Route::get('list',[DeviceController::class,'list']);
+// Route::get('list/{id}',[DeviceController::class,'listParams']);
+
+
+// Route::post('add',[DeviceController::class,'add']);
+// Route::put('update',[DeviceController::class,'update']);
+
+// Route::get('search/{name}',[DeviceController::class,'search']);
+// Route::delete('delete/{id}',[DeviceController::class,'delete']);
+
+
+// Route::post('save',[DeviceController::class,'testData']);
+
+//Route::apiResource("member",[MemberController::class]);
+
+Route::post("login",[UserController::class,'index']);
